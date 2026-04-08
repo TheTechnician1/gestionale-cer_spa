f8f2ed0bbc79046a0b47f7ab729897efdd8ff090
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-08 11:04:18 +0200

    [FEAT] Aggiunta model CER, e richiamato su service CER l'interfaccia

---
+++ src/app/core/interfaces/cer.model.ts
@@ -0,0 +1,12 @@
+export interface CER {
+    id: string,
+    ragione_sociale: string,
+    codice_fiscale: string,
+    comune_legale: string,
+    provincia_legale: string,
+    regione_legale: string,
+    forma_giuridica: string,
+    email: string,
+    pec: string,
+    referente: string
+}
--- src/app/core/services/cer.service.ts
+++ src/app/core/services/cer.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CER } from '../interfaces/cer.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class CERService {
 
   constructor() { }
+
+  cer?: CER;
+
+
 }
