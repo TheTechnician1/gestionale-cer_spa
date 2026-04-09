4d13c060b6e35346ceab176878cfc1f857513519
attil44 <francescoio476@gmail.com>
2026-04-09 16:52:07 +0200

    aggiornati le label del component login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const FULL_LAYOUT_ROUTES: Routes = [
 ];
 
 export const routes: Routes = [
-  { path: "", component: FullLayoutComponent, data: { title: "content Views" }, children: FULL_LAYOUT_ROUTES /* canActivate: [AuthGuard] */ },
   { path: "login", component: LoginComponent },
+  { path: "", component: FullLayoutComponent, data: { title: "content Views" }, children: FULL_LAYOUT_ROUTES /* canActivate: [AuthGuard] */ },
+  
 ];
