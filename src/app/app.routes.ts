4b065ac147b75838c80705f411e532f6a087b265
marcsilp <marsil93@outlook.it>
2026-04-09 12:31:28 +0200

    [ADD] Aggiunto form login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from "@angular/router";
 import { FullLayoutComponent } from "./core/layout/full-layout.component";
 import { AuthGuard } from "./core/guard/auth.guard";
 import { RegistrazioneUtenteComponent } from "./core/component/login/registrazione-utente/registrazione-utente.component";
+import { LoginComponent } from "./core/component/login/login/login.component";
 
 export const FULL_LAYOUT_ROUTES: Routes = [
-  { path: "registrazione", component: RegistrazioneUtenteComponent }
+  { path: "registrazione", component: RegistrazioneUtenteComponent 
+  },
+  { path: "login", component: LoginComponent }
 ];
 
 export const routes: Routes = [{ path: "", component: FullLayoutComponent, data: { title: "content Views" }, children: FULL_LAYOUT_ROUTES /* canActivate: [AuthGuard] */ }];
--- src/app/core/component/login/login/login.component.ts
+++ src/app/core/component/login/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
 
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log("LoginComponent initialized");
+
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(8)],
+      rememberME: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+  }
+}
 }
