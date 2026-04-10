163f666a28caccd1e704ca640186e0e27e46d913
attil44 <francescoio476@gmail.com>
2026-04-10 10:00:13 +0200

    tolti i required inutili e aggiunto commento alle rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ export const FULL_LAYOUT_ROUTES: Routes = [
 ];
 
 export const routes: Routes = [
+  //{ path: "", redirectTo: "login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "", component: FullLayoutComponent, data: { title: "content Views" }, children: FULL_LAYOUT_ROUTES /* canActivate: [AuthGuard] */ },
   
