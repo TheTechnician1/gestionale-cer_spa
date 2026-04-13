5570d40e5314e16cc7a39c2313f1cd23e4b84294
marcsilp <marsil93@outlook.it>
2026-04-13 12:08:42 +0200

    [FIX] Fixato dettaglio-cer.component e collegamento bottoe cer

--- src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
+++ src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
@@ -1,8 +1,4 @@
-import { getTreeControlFunctionsMissingError } from '@angular/cdk/tree';
 import { Component } from '@angular/core';
-import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Stato } from '../../interfaces/stato.model';
-
 
 @Component({
   selector: 'app-dettaglio-cer',
@@ -10,34 +6,5 @@ import { Stato } from '../../interfaces/stato.model';
   styleUrls: ['./dettaglio-cer.component.scss']
 })
 export class DettaglioCerComponent {
-  cerForm!:FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    
-    this.cerForm =this.fb.group({
-      regioneSociale: ['', Validators.required],
-      partitaIva: ['', Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")],
-      codiceFiscale: ['', Validators.required], 
-      formaGiuridica: ['', Validators.required],
-      telefono: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      pec: ['', [Validators.required, Validators.email]],
-      sitoWeb: ['', Validators.required],
-      nomeCognomeReferente: ['', Validators.required],
-      numeroCabine: ['', Validators.required],
-      viaSedeLegale: ['', Validators.required],
-      cap: ['', Validators.required],
-      comuneSedeLegale: ['', Validators.required],
-      provinciaSedeLegale: ['', Validators.required],
-      regioneSedeLegale: ['', Validators.required],
-
-      statoCer: ['', Validators.required]
-    });
-  }
-
-  hide = true;
-  stati: Stato[] = [
-    { value: 'attivo', viewValue: 'Attivo'},
-    { value: 'non attivo', viewValue: 'Non attivo'}
-  ]
+  
 }
