fe65ee3f204f121560113648a8799a1d643d9505
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-13 16:00:49 +0200

    [FIX] Fixato dettaglio

--- src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
+++ src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Stato } from '../../interfaces/stato.model';
 
 @Component({
   selector: 'app-dettaglio-cer',
@@ -7,38 +8,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./dettaglio-cer.component.scss']
 })
 export class DettaglioCerComponent implements OnInit {
-  
-  cerForm!: FormGroup;
-  
+  cerForm!:FormGroup;
+
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void{
-    this.cerForm = this.fb.group({
-        regioneSociale: [''],
-        partitaIva: [''],
-        codiceFiscale: [''],
-        formaGiuridica: [''],
-        telefono: [''],
-        email: [''],
-        pec: [''],
-        sitoWeb: [''],
-        nomeCognomeReferente: [''],
-        numeroCabine: [''],
-        viaSedeLegale: [''],
-        cap: [''],
-        comuneSedeLegale: [''],
-        provinciaSedeLegale: [''],
-        regioneSedeLegale: [''],
-        statoCer: [''],
-        iscrizioneRunts: [false],
-        terzoSettore: [false],
-        progettiInclusioneSociale: [false],
-        areeMontaneOInterne: [false],
-        progettiCambiamentiClimatici: [false]
+  ngOnInit(): void {
+    this.cerForm =this.fb.group({
+      ragioneSociale: ['', [Validators.required]],
+      partitaIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
+      codiceFiscale: ['', [Validators.required]],
+      formaGiuridica: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      pec: ['', [Validators.required, Validators.email]],
+      sitoWeb: ['', [Validators.required]],
+      nomeCognomeReferente: ['', [Validators.required]],
+      numeroCabine: ['', [Validators.required]],
+      viaSedeLegale: ['', [Validators.required]],
+      cap: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      comuneSedeLegale: ['', [Validators.required]],
+      provinciaSedeLegale: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+      regioneSedeLegale: ['', [Validators.required]],
+      statoCer: ['', [Validators.required]],
+      iscrizioneRunts: [false],
+      terzoSettore: [false],
+      progettiInclusioneSociale: [false],
+      areeMontaneInterne: [false],
+      progettiCambiamentiClimatici: [false]
     });
-
     this.cerForm.disable();
   }
 
-
+  stati: Stato[] = [
+    { value: 'attivo', viewValue: 'Attivo'},
+    { value: 'noattivo', viewValue: 'Non Attivo'}
+  ]
 }
