ed6b74eb2c4038e78fc0c7441bcafabc8d0d8a58
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-13 11:54:27 +0200

    [ADD] Implementate tutte le CRUD del Service sul Component Utente

--- src/app/core/component/utente/utente.component.ts
+++ src/app/core/component/utente/utente.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtenteService } from '../../services/utente.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-utente',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./utente.component.scss']
 })
 export class UtenteComponent {
+  constructor(private utenteService: UtenteService, private router: ActivatedRoute, private route: Router) { }
 
+  utenti: any[] = [];
+
+  utente: any = {
+    id_utente: 0,
+    nome_utente: '',
+    cognome_utente: '',
+    codice_fiscale: '',
+    email: '',
+    password: '',
+    numero_telefono: '',
+    ruolo: ''
+  }
+
+  ngOnInit() {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.loadUtente(parseInt(id!));
+  }
+
+  loadUtente(id: number) {
+    this.utenteService.getUtente(id);
+  }
+
+  loadUtenti() {
+    this.utenti = this.utenteService.getUtenti();
+  }
+
+  getUtenti() {
+    return this.utenti;
+  }
+
+  getUtente(id: number) {
+    this.utente = this.utenti.filter(p => p.id_utente === id)[0];
+    return this.utente;
+  }
+
+  createUtente(user: any){
+    this.utente = this.utenteService.createUtente(user);
+    this.route.navigate(['/']);
+  }
+
+  editUtente(user: any) {
+    this.utente = { ...user };
+    this.route.navigate(['/']);
+  }
+
+  updatePersonaggio() {
+    this.utenteService.editUtente(this.utente);
+    this.loadUtente(this.utente.id);
+  }
+
+  deleteUtente(id: number) {
+    this.utenteService.deleteUtente(id);
+  }
 }
