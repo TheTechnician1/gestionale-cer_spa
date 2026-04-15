e3c535227fb21eea6ff34f0b0dde918f2dc59dfa
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-15 09:26:11 +0200

    [FIX] Fix menu a tendina, eliminate le varie parti che non tornavano (impostazioni, anagrafiche, e lista impianti)

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -12,25 +12,9 @@ interface NavItem {
 const NAV_ITEMS: NavItem[] = [
   { label: "Dashboard", icon: "dashboard", route: "/dashboard" },
   { label: "Registrazione", icon: "person_add", route: "/registrazione" },
-  {
-    label: "Anagrafiche",
-    icon: "badge",
-    children: [
-      { label: "Clienti", icon: "groups", route: "/anagrafiche/clienti" },
-      { label: "Fornitori", icon: "local_shipping", route: "/anagrafiche/fornitori" },
-    ],
-  },
   { label: "Comunità Energetiche", icon: "factory" , route: "/cer"},
   { label: "Configurazioni", icon: "cabin", route: "/configurazioni"},
-  {
-    label: "Impianti",
-    icon: "bolt",
-    children: [
-      { label: "Monitoraggio", icon: "monitor_heart", route: "/impianti/monitoraggio" },
-      { label: "Manutenzioni", icon: "build", route: "/impianti/manutenzioni" },
-    ],
-  },
-  { label: "Impostazioni", icon: "settings", route: "/impostazioni" },
+  { label: "Impianti", icon: "bolt", route: "/impianti" },
 ];
 
 @Component({
