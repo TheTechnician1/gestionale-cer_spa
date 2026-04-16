d0770465b1755121d3c434e5d58f73046ed63b75
marcsilp <marsil93@outlook.it>
2026-04-16 11:56:58 +0200

    [FIX] Fix dati-energetici

--- src/app/core/component/dati-energetici/dati-energetici.component.ts
+++ src/app/core/component/dati-energetici/dati-energetici.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatiEnergetici } from '../../interfaces/dati-energetici.model';
 
 @Component({
   selector: 'app-dati-energetici',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./dati-energetici.component.scss']
 })
 export class DatiEnergeticiComponent {
+  tableDat: string[] = ['id_dati', 'energia_prodotta', 'energia_prelevata', 'azioni'];
 
+  datiEnergetici: DatiEnergetici[] = [
+   { id_dati: "14",
+     id_cer: "",
+     id_config: "",
+     anno: "",
+     energia_prodotta: 45,
+     energia_prelevata: 67,
+     energia_immessa: 32,
+     energia_condivisa: 57,
+     energia_autoconsumata: 23,
+     tariffa_premio: 56,
+     corrispettivo_premio: 32,
+     riduzione_emissione: ""
+    }
+  ]
+
+  dataSource = this.datiEnergetici;
+
+  deleteDat() {
+
+  }
 }
