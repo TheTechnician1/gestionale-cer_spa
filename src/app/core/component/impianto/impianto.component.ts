8cce6c44e5e7fa894ec4a55a7dddbba9021444bc
marcsilp <marsil93@outlook.it>
2026-04-16 09:31:49 +0200

    [FIX] Fix impianto.component

--- src/app/core/component/impianto/impianto.component.ts
+++ src/app/core/component/impianto/impianto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Impianto } from '../../interfaces/impianto.model';
 
 @Component({
   selector: 'app-impianto',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./impianto.component.scss']
 })
 export class ImpiantoComponent {
+  tableImp: string[] = ['id_impianto', 'codice_cabina', 'data_entrata_esercizio', 'azioni'];
+  
+  impianto: Impianto[] = [
+    { id_impianto: '37', 
+      id_configurazione: "", 
+      codice_cabina: "7d8h2", 
+      data_entrata_esercizio: "12/3/26",
+      flag_impianto: false,
+      tipologia_impianto: "",
+      potenza_nominale: "",
+      presenza_accumulo: "",
+      capacita_accumulo: "",
+      tipologia_produttore: "",
+      categoria_produttore: "",
+      ubicazione_impianto: [
+        {
+          regione: "",
+          provincia: "",
+          comune: "",
+          indirizzo: "",
+          numero_civico: "",
+          cap: "",
+          tipologia_sito: ""
+        }
+      ]
+    }
+  ]
 
+  dataSource = this.impianto;
+  
+
+  deleteImp() {
+  
+  }
 }
