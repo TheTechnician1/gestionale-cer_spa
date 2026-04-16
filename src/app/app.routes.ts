8e6c230ab3529308eeb1e04e517f8c292e5c803e
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-16 12:48:55 +0200

    [FIX] Fix delle rotte, aggiunte alcune mancanti come dati-energetici ed impianti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,20 +11,35 @@ import { DashboardComponent } from "./core/component/dashboard/dashboard.compone
 import { ProfiloUtenteComponent } from "./core/component/profilo-utente/profilo-utente.component";
 import { InserimentoCerComponent } from "./core/component/inserimento-cer/inserimento-cer.component";
 import { HomeComponent } from "./core/component/home/home.component";
+import { ImpiantoComponent } from "./core/component/impianto/impianto.component";
+import { InserimentoImpiantoComponent } from "./core/component/inserimento-impianto/inserimento-impianto.component";
+import { DatiEnergeticiComponent } from "./core/component/dati-energetici/dati-energetici.component";
 
 export const FULL_LAYOUT_ROUTES: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "", redirectTo: "home", data: { role: ["ADMIN", "GEST", "GUEST"] }, pathMatch: "full" },
   { path: "home", component: HomeComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard], pathMatch: 'full' },
-  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: "dashboard", component: DashboardComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard] },
   { path: "registrazione", component: RegistrazioneUtenteComponent, data: { role: ["ADMIN"] }, canActivate: [AuthGuard]},
-  { path: "cer", component: CERComponent, canActivate: [AuthGuard],
+  { path: "cer", component: CERComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard],
     children: [
       { path: "inserimento-cer", component: InserimentoCerComponent, data: { role: ["ADMIN"] }, canActivate: [AuthGuard] },
       { path: "dettaglio-cer", component: DettaglioCerComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard] },
       { path: "modifica-cer", component: ModificaCerComponent, data: { role: ["ADMIN"] }, canActivate: [AuthGuard] }
     ]
   },
-  { path: "configurazioni", component: ConfigurazioneComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard] },
+  { path: "configurazioni", component: ConfigurazioneComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard],
+    children: [
+      { path: "inserimento-configurazioni", component: HomeComponent, data: { role: ["ADMIN", "GEST"] }, canActivate: [AuthGuard] },
+      { path: "modifica-configurazioni", component: HomeComponent, data: { role: ["ADMIN", "GEST"] }, canActivate: [AuthGuard] },
+    ]
+  },
+  { path: "impianto", component: ImpiantoComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard],
+    children: [
+      { path: "inserimento-impianto", component: InserimentoImpiantoComponent, data: { role: ["ADMIN", "GEST"] }, canActivate: [AuthGuard] },
+      { path: "modifica-impianto", component: HomeComponent, data: { role: ["ADMIN", "GEST"] }, canActivate: [AuthGuard] },
+    ]
+  },
+  { path: "dati-energetici", component: DatiEnergeticiComponent, data: { role: ["ADMIN", "GEST", "GUEST"] }, canActivate: [AuthGuard] },
   { path: "profilo-utente", component: ProfiloUtenteComponent, data: { role: ["ADMIN", "GEST"] }, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent },
   { path: "**",redirectTo: "login" },
