15fe19172ff5629fb3100db9b757b7997c086228
marcsilp <marsil93@outlook.it>
2026-04-20 15:18:27 +0200

    [FIX] Fix modifica-configurazione.component.ts e modifica-dati-energetici.component.ts

--- src/app/core/component/modifica-configurazione/modifica-configurazione.component.ts
+++ src/app/core/component/modifica-configurazione/modifica-configurazione.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica-configurazione',
   templateUrl: './modifica-configurazione.component.html',
   styleUrls: ['./modifica-configurazione.component.scss']
 })
-export class ModificaConfigurazioneComponent {
+export class ModificaConfigurazioneComponent implements OnInit {
 
+  configForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.configForm = this.fb.group({
+      codice_cabina: ['', [Validators.required]],
+      anno_attivazione: ['', [Validators.required]]
+    });
+    this.configForm.enable();
+  }
 }
--- src/app/core/component/modifica-dati-energetici/modifica-dati-energetici.component.ts
+++ src/app/core/component/modifica-dati-energetici/modifica-dati-energetici.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica-dati-energetici',
   templateUrl: './modifica-dati-energetici.component.html',
   styleUrls: ['./modifica-dati-energetici.component.scss']
 })
-export class ModificaDatiEnergeticiComponent {
+export class ModificaDatiEnergeticiComponent implements OnInit {
 
+  datiEnergeticiForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.datiEnergeticiForm = this.fb.group({
+     id_cer: ['', [Validators.required]],
+     id_config: ['', [Validators.required]],
+     anno: ['', [Validators.required]],
+     energia_prodotta: ['', [Validators.required]],
+     energia_prelevata: ['', [Validators.required]],
+     energia_immessa: ['', [Validators.required]],
+     energia_condivisa: ['', [Validators.required]],
+     energia_autoconsumata: ['', [Validators.required]],
+     tariffa_premio: ['', [Validators.required]],
+     corrispettivo_premio: ['', [Validators.required]],
+     riduzione_emissione: ['', [Validators.required]]
+    });
+    this.datiEnergeticiForm.enable();
+  }
 }
