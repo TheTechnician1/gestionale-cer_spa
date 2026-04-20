2bd9714aefe90f2739ee9b9481cd6280c4ba2039
marcsilp <marsil93@outlook.it>
2026-04-20 15:05:16 +0200

    [FEAT] Aggiunto dettaglio-impianto

---
+++ src/app/core/component/dettaglio-impianto/dettaglio-impianto.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DettaglioImpiantoComponent } from './dettaglio-impianto.component';
+
+describe('DettaglioImpiantoComponent', () => {
+  let component: DettaglioImpiantoComponent;
+  let fixture: ComponentFixture<DettaglioImpiantoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DettaglioImpiantoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DettaglioImpiantoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
