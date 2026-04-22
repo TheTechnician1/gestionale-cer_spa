ecd7ce9826b2f7f2617ed1ec85ef7e7b3fdf1aa1
v.lucente <v.lucente@majorbit.com>
2026-04-22 15:13:23 +0200

    Fix routing per componenti con stesso path tramite l'utilizzo di lazyLoading
    Implementati metodi in api.service per chiamate che ritornano json o text

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -12,26 +12,69 @@ export class ApiService {
 
   constructor(private http: HttpClient, private api: StorageService) {}
 
-  get<T>(path: string, params?: Record<string, string | number | boolean>): Observable<T> {
-    return this.http.get<T>(this.buildUrl(path), { params: this.buildParams(params) });
+  get<T>(path: string, params?: any): Observable<T> {
+    return this.request<T>('GET', path, { params });
   }
 
   postLogin<T>(path: string, body: any): Observable<T> {
-    return this.http.post<T>(this.buildUrl(path), body);
+    return this.request<T>('POST', path, body);
   }
 
   post<T>(path: string, body: any): Observable<T> {
     body.emailUtenteLoggato = this.api.getLocal<UtenteLogin>("utente")?.mail;
-    return this.http.post<T>(this.buildUrl(path), body);
+    return this.request<T>('POST', path, body);
+  }
+
+  postText(path: string, body: any): Observable<string> {
+    body.emailUtenteLoggato = this.api.getLocal<UtenteLogin>("utente")?.mail;
+    return this.request('POST', path, body, 'text');
   }
 
   put<T>(path: string, body: any): Observable<T> {
     body.emailUtenteLoggato = this.api.getLocal<UtenteLogin>("utente")?.mail;
-    return this.http.put<T>(this.buildUrl(path), body);
+    return this.request<T>('PUT', path, body);
   }
 
-  delete<T>(path: string, params?: Record<string, string | number | boolean>): Observable<T> {
-    return this.http.delete<T>(this.buildUrl(path), { params: this.buildParams(params) });
+  putText(path: string, body: any): Observable<string> {
+    body.emailUtenteLoggato = this.api.getLocal<UtenteLogin>("utente")?.mail;
+    return this.request('PUT', path, body, 'text');
+  }
+
+  delete<T>(path: string, params?: any): Observable<T> {
+    return this.request<T>('DELETE', path, { params });
+  }
+
+  private request<T>(
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    path: string,
+    body?: any,
+    responseType: 'json' | 'text' = 'json',
+    params?: Record<string, any>
+  ): Observable<any> {
+
+    const url = this.buildUrl(path);
+
+    if (body) {
+      body.emailUtenteLoggato =
+        this.api.getLocal<UtenteLogin>("utente")?.mail;
+    }
+
+    const options: any = {
+      body,
+      params: this.buildParams(params),
+      responseType
+    };
+
+    switch (method) {
+      case 'GET':
+        return this.http.get<T>(url, body);
+      case 'POST':
+        return this.http.post<T>(url, body, options);
+      case 'PUT':
+        return this.http.put<T>(url, body, options);
+      case 'DELETE':
+        return this.http.delete<T>(url, options);
+    }
   }
 
   private buildUrl(path: string): string {
