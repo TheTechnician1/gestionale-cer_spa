fab9f2b099ea32b59bee60f08e662bbb9f7f5731
marcsilp <marsil93@outlook.it>
2026-04-23 15:04:38 +0200

    [FIX] Fixato dettaglio-cer

--- src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
+++ src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Stato } from '../../interfaces/stato.model';
 
@@ -7,38 +7,30 @@ import { Stato } from '../../interfaces/stato.model';
   templateUrl: './dettaglio-cer.component.html',
   styleUrls: ['./dettaglio-cer.component.scss']
 })
-export class DettaglioCerComponent implements OnInit {
+export class DettaglioCerComponent {
   cerForm!:FormGroup;
-
+  
   constructor(private fb: FormBuilder) {}
-
+  
   ngOnInit(): void {
-    this.cerForm = this.fb.group({
-      ragioneSociale: ['', [Validators.required]],
-      partitaIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
-      codiceFiscale: ['', [Validators.required]],
+    this.cerForm =this.fb.group({
+      ragSociale: ['', [Validators.required]],
+      pIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
+      codFisc: ['', [Validators.required]],
       formaGiuridica: ['', [Validators.required]],
       telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       email: ['', [Validators.required, Validators.email]],
       pec: ['', [Validators.required, Validators.email]],
       sitoWeb: ['', [Validators.required]],
-      nomeCognomeReferente: ['', [Validators.required]],
-      numeroCabine: ['', [Validators.required]],
-      viaSedeLegale: ['', [Validators.required]],
-      cap: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      comuneSedeLegale: ['', [Validators.required]],
-      provinciaSedeLegale: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-      regioneSedeLegale: ['', [Validators.required]],
+      referente: ['', [Validators.required]],
+      comune: ['', [Validators.required]],
+      provincia: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+      regione: ['', [Validators.required]],
       statoCer: ['', [Validators.required]],
-      iscrizioneRunts: [false],
-      terzoSettore: [false],
-      progettiInclusioneSociale: [false],
-      areeMontaneInterne: [false],
-      progettiCambiamentiClimatici: [false]
     });
     this.cerForm.disable();
   }
-
+  
   stati: Stato[] = [
     { value: 'attivo', viewValue: 'Attivo'},
     { value: 'noattivo', viewValue: 'Non Attivo'}
