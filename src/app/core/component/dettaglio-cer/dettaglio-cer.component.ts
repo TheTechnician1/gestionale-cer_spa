11369f57807f7354968c0af9656da65b261e3b2c
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-23 15:18:05 +0200

    [FIX] Rimesso OnInit perche  Marco l'ha tolto senza motivo

--- src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
+++ src/app/core/component/dettaglio-cer/dettaglio-cer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Stato } from '../../interfaces/stato.model';
 import { CERService } from '../../services/cer.service';
