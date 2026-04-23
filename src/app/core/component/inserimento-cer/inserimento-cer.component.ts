51647326d32a66f09acb2b44fe972b687d2eaeee
marcsilp <marsil93@outlook.it>
2026-04-23 15:08:36 +0200

    [FIX] Fixato inserimento-cer

--- src/app/core/component/inserimento-cer/inserimento-cer.component.ts
+++ src/app/core/component/inserimento-cer/inserimento-cer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Stato } from '../../interfaces/stato.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-inserimento-cer',
@@ -10,34 +11,23 @@ import { Stato } from '../../interfaces/stato.model';
 export class InserimentoCerComponent {
   cerForm!:FormGroup;
   
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
   
   ngOnInit(): void {
     this.cerForm =this.fb.group({
-      ragioneSociale: ['', [Validators.required]],
-      partitaIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
-      codiceFiscale: ['', [Validators.required]],
+      ragSociale: ['', [Validators.required]],
+      pIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
+      codFisc: ['', [Validators.required]],
       formaGiuridica: ['', [Validators.required]],
       telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       email: ['', [Validators.required, Validators.email]],
       pec: ['', [Validators.required, Validators.email]],
       sitoWeb: ['', [Validators.required]],
-      nomeCognomeReferente: ['', [Validators.required]],
-      numeroCabine: ['', [Validators.required]],
-      viaSedeLegale: ['', [Validators.required]],
-      cap: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      comuneSedeLegale: ['', [Validators.required]],
-      provinciaSedeLegale: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-      regioneSedeLegale: ['', [Validators.required]],
+      referente: ['', [Validators.required]],
+      comune: ['', [Validators.required]],
+      provincia: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+      regione: ['', [Validators.required]],
       statoCer: ['', [Validators.required]],
-      attoCosttutivo: [null],
-      statutoStipulato: [null],
-      regolamentoCer: [null],
-      iscrizioneRunts: [false],
-      terzoSettore: [false],
-      progettiInclusioneSociale: [false],
-      areeMontaneInterne: [false],
-      progettiCambiamentiClimatici: [false]
     });
     this.cerForm.enable();
   }
@@ -46,5 +36,36 @@ export class InserimentoCerComponent {
     { value: 'attivo', viewValue: 'Attivo'},
     { value: 'noattivo', viewValue: 'Non Attivo'}
   ]
+
+  submitted = false;
+
+  submit() {
+    this.submitted = true;
+    
+    if (this.cerForm.invalid) {
+      // return;
+      this.cerForm.markAllAsTouched();
+
+      this.snackBar.open(
+        'Compila tutti i campi obbligatori correttamente',
+        // 'Chiudi',
+        // {
+        //   duration: 3000
+        // }
+      );
+
+    // console.log(this.cerForm.value);
+    return;
+    }
+
+    this.snackBar.open(
+      'Inserimento completato!',
+      // 'OK',
+      // {
+      //   duration: 2000
+      // }
+    );
+    console.log(this.cerForm.value);
+  }
   
 }
