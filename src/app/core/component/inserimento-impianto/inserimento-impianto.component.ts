4346428cd2ec00382cef16ebc8f7e559af2136ad
marcsilp <marsil93@outlook.it>
2026-04-23 15:11:02 +0200

    [FIX] Fixato inserimento-impianto, aggiunto gestione errori

--- src/app/core/component/inserimento-impianto/inserimento-impianto.component.ts
+++ src/app/core/component/inserimento-impianto/inserimento-impianto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-inserimento-impianto',
@@ -10,7 +11,7 @@ export class InserimentoImpiantoComponent implements OnInit {
 
   impiantiForm!: FormGroup;
   
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
   
   ngOnInit(): void {
     this.impiantiForm = this.fb.group({
@@ -34,4 +35,25 @@ export class InserimentoImpiantoComponent implements OnInit {
     });
     this.impiantiForm.enable();
   }
+
+  submitted = false;
+
+  submit() {
+    this.submitted = true;
+
+    if (this.impiantiForm.invalid) {
+      this.impiantiForm.markAllAsTouched();
+
+      this.snackBar.open(
+        'Compila tutti i campi obbligatori correttamente'
+      )
+      return;
+    }
+
+    this.snackBar.open(
+      'Inserimento completato!'
+    );
+    console.log(this.impiantiForm.value);
+  }
+
 }
