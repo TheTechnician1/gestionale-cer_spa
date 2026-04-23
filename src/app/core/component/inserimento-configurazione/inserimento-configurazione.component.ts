54805e7660e1918f74ffd1e8f44800813eaa4bd2
marcsilp <marsil93@outlook.it>
2026-04-23 15:09:53 +0200

    [FIX] Fixato inserimento-configurazione

--- src/app/core/component/inserimento-configurazione/inserimento-configurazione.component.ts
+++ src/app/core/component/inserimento-configurazione/inserimento-configurazione.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-inserimento-configurazione',
@@ -10,13 +11,34 @@ export class InserimentoConfigurazioneComponent implements OnInit {
 
   configForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.configForm = this.fb.group({
-      codice_cabina: ['', [Validators.required]],
-      anno_attivazione: ['', [Validators.required]]
+      codiceCabina: ['', [Validators.required]],
+      annoAttivazione: ['', [Validators.required]]
     });
     this.configForm.enable();
   }
+
+  submitted = false;
+
+  submit() {
+    this.submitted = true;
+
+    if (this.configForm.invalid) {
+      this.configForm.markAllAsTouched();
+
+      this.snackBar.open(
+        'Compila tutti i campi obbligatori correttamente'
+      );
+      return;
+    }
+
+    this.snackBar.open(
+      'Inserimento completato!'
+    );
+    console.log(this.configForm.value);
+  }
+
 }
