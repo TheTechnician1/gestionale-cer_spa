0624710f63d7917a27c7ecbb0864e33f4af8e575
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-24 14:21:17 +0200

    [FIX] Fix configurazione per funzionare col dettaglio e modifica

--- src/app/core/component/modifica-configurazione/modifica-configurazione.component.ts
+++ src/app/core/component/modifica-configurazione/modifica-configurazione.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Configurazione } from '../../interfaces/configurazione.model';
+import { ConfigurazioneService } from '../../services/configurazione.service';
 
 @Component({
   selector: 'app-modifica-configurazione',
@@ -8,37 +11,55 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./modifica-configurazione.component.scss']
 })
 export class ModificaConfigurazioneComponent implements OnInit {
-
+  constructor(private fb: FormBuilder, private configService: ConfigurazioneService, private snackBar: MatSnackBar, private route: ActivatedRoute) {}
+  config!: Configurazione;
   configForm!: FormGroup;
-
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
+  submitted = false;
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.loadConfig(parseInt(id!));
     this.configForm = this.fb.group({
       codiceCabina: ['', [Validators.required]],
       annoAttivazione: ['', [Validators.required]]
     });
+    this.loadConfig(parseInt(id!));
     this.configForm.enable();
   }
 
-  submitted = false;
+  loadConfig(id: number) {
+    this.configService.getConfigurazione(id).subscribe({
+      next: (config) => {
+      console.log("Peppeeeee", config);
+      this.config = config;
+      this.configForm.patchValue(config);
+      },
+      error: (error) => {
+        console.error("Login error", error);
+      }
+    });
+  }
+
+  editConfig(payload: any) {
+    this.configService.editConfigurazione(payload).subscribe({
+      next: (res) => {
+        this.snackBar.open('Modifica completata!');
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 
   submit() {
     this.submitted = true;
-
     if (this.configForm.invalid) {
       this.configForm.markAllAsTouched();
-
-      this.snackBar.open(
-        'Compila tutti i campi obbligatori correttamente'
-      );
+      this.snackBar.open('Compila tutti i campi obbligatori correttamente');
+      console.log(this.configForm);
       return;
     }
-
-    this.snackBar.open(
-      'Inserimento completato!'
-    );
-    console.log(this.configForm.value);
+    const payload = { ...this.configForm.value, id: this.config.idConfigurazione }
+    this.editConfig(payload);
   }
-  
 }
