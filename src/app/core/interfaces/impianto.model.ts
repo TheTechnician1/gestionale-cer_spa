a22ed2d751c6087a86d327ae56bf41ecb9d71b2b
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-24 10:22:16 +0200

    [UPD] Aggiornato Impianto per coincidere col backend

--- src/app/core/interfaces/impianto.model.ts
+++ src/app/core/interfaces/impianto.model.ts
@@ -2,13 +2,19 @@ export interface Impianto {
   idImpianto: number | null,
   idConfigurazione: number | null,
   flgEsercizio: string | null,
-  dataEserc: Date | null,
-  codTipologia: string | null,
+  dataAttivazione: Date | null,
+  partitaIva: string | null,
+  regione: string | null,
+  comune: string | null,
+  provincia: string | null,
+  codiceCabina: string | null,
+  codiceTipologia: string | null,
   preNom: number | null,
   flgAccumulo: string | null,
   capAccumulo: number | null,
   tipoProduttore: string | null,
-  codCatProd: string | null,
+  codCategoriaProduttore: string | null,
+  codInstallazione: string | null,
   flg_cancellazione: string | null,
   spec_tipologia: string | null,
   spec_cat_produttore: string | null,
@@ -31,13 +37,19 @@ export class ImpiantoModel implements Impianto {
   idImpianto: number | null;
   idConfigurazione: number | null;
   flgEsercizio: string | null;
-  dataEserc: Date | null;
-  codTipologia: string | null;
+  dataAttivazione: Date | null;
+  partitaIva: string | null;
+  regione: string | null;
+  comune: string | null;
+  provincia: string | null;
+  codiceCabina: string | null;
+  codiceTipologia: string | null;
   preNom: number | null;
   flgAccumulo: string | null;
   capAccumulo: number | null;
   tipoProduttore: string | null;
-  codCatProd: string | null;
+  codCategoriaProduttore: string | null;
+  codInstallazione: string | null;
   flg_cancellazione: string | null;
   spec_tipologia: string | null;
   spec_cat_produttore: string | null;
@@ -49,13 +61,19 @@ export class ImpiantoModel implements Impianto {
     this.idImpianto = data?.idImpianto ?? null;
     this.idConfigurazione = data?.idConfigurazione ?? null;
     this.flgEsercizio = data?.flgEsercizio ?? null;
-    this.dataEserc = data?.dataEserc ?? null;
-    this.codTipologia = data?.codTipologia ?? null;
+    this.dataAttivazione = data?.dataAttivazione ?? null;
+    this.partitaIva = data?.partitaIva ?? null;
+    this.codiceTipologia = data?.codiceTipologia ?? null;
+    this.regione = data?.regione ?? null;
+    this.comune = data?.comune ?? null;
+    this.provincia = data?.provincia ?? null;
+    this.codiceCabina = data?.codiceCabina ?? null;
     this.preNom = data?.preNom ?? null;
     this.flgAccumulo = data?.flgAccumulo ?? null;
     this.capAccumulo = data?.capAccumulo ?? null;
     this.tipoProduttore = data?.tipoProduttore ?? null;
-    this.codCatProd = data?.codCatProd ?? null;
+    this.codCategoriaProduttore = data?.codCategoriaProduttore ?? null;
+    this.codInstallazione = data?.codInstallazione ?? null;
     this.flg_cancellazione = data?.flg_cancellazione ?? null;
     this.spec_tipologia = data?.spec_tipologia ?? null;
     this.spec_cat_produttore = data?.spec_cat_produttore ?? null;
