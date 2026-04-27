d965342754a3aa8dade84a89db1a2f11e3ad9dca
Michele Aliffi <michelealiffi890@gmail.com>
2026-04-27 15:27:39 +0200

    [UPD] Aggiornato il modifica-cer per funzionare perfettamente col backend

--- src/app/core/component/modifica-cer/modifica-cer.component.ts
+++ src/app/core/component/modifica-cer/modifica-cer.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Stato } from '../../interfaces/stato.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CERService } from '../../services/cer.service';
+import { ActivatedRoute } from '@angular/router';
+import { CER } from '../../interfaces/cer.model';
 
 @Component({
   selector: 'app-modifica-cer',
@@ -10,40 +13,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 
 export class ModificaCerComponent {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private cerService: CERService, private route: ActivatedRoute) {}
+
+  cer?: CER;
   cerForm!:FormGroup;
-
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
-
-  ngOnInit(): void {
-    this.cerForm =this.fb.group({
-      ragSociale: ['', [Validators.required]],
-      pIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
-      codFisc: ['', [Validators.required]],
-      formaGiuridica: ['', [Validators.required]],
-      telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      email: ['', [Validators.required, Validators.email]],
-      pec: ['', [Validators.required, Validators.email]],
-      sitoWeb: ['', [Validators.required]],
-      referente: ['', [Validators.required]],
-      // numeroCabine: ['', [Validators.required]],
-      // viaSedeLegale: ['', [Validators.required]],
-      // cap: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
-      comune: ['', [Validators.required]],
-      provincia: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
-      regione: ['', [Validators.required]],
-      statoCer: ['', [Validators.required]],
-      // attoCosttutivo: [null],
-      // statutoStipulato: [null],
-      // regolamentoCer: [null],
-      // iscrizioneRunts: [false],
-      // terzoSettore: [false],
-      // progettiInclusioneSociale: [false],
-      // areeMontaneInterne: [false],
-      // progettiCambiamentiClimatici: [false]
-    });
-    this.cerForm.enable();
-  }
-
   stati: Stato[] = [
     { value: 'attivo', viewValue: 'Attivo'},
     { value: 'noattivo', viewValue: 'Non Attivo'}
@@ -51,9 +24,58 @@ export class ModificaCerComponent {
 
   submitted = false;
 
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.cerForm = this.fb.group({
+      ragSociale: ['', [Validators.required]],
+      pIva: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9\d#@èé€çòà°ù§ì£$^!(/>{}'|/`~<)-_%*?&]{8,64}$")]],
+      codFiscale: ['', [Validators.required]],
+      formaGiuridica: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      pec: ['', [Validators.required, Validators.email]],
+      sitoWeb: ['', [Validators.required]],
+      referente: ['', [Validators.required]],
+      comuneLegale: ['', [Validators.required]],
+      provinciaLegale: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+      regioneLegale: ['', [Validators.required]],
+      flgCancelllazione: ['', [Validators.required]],
+    });
+    this.cerForm.enable();
+    this.loadCER(parseInt(id!));
+  }
+
+  loadCER(id: number) {
+    this.cerService.getCER(id).subscribe({
+      next: (cer) => {
+      this.cer = cer[0];
+
+      if(this.cerForm) {
+        this.cerForm.patchValue({
+          ragSociale: this.cer.ragSociale,
+          pIva: this.cer.pIva,
+          codFiscale: this.cer.codFiscale,
+          formaGiuridica: this.cer.formaGiuridica?.descrizione,
+          telefono: this.cer.telefono,
+          email: this.cer.email,
+          pec: this.cer.pec,
+          sitoWeb: this.cer.sitoWeb,
+          referente: this.cer.referente,
+          comuneLegale: this.cer.comuneLegale?.descrizione,
+          provinciaLegale: this.cer.provinciaLegale?.descrizione,
+          regioneLegale: this.cer.regioneLegale?.descrizione,
+          flgCancellazione: this.cer.flgCancellazione
+        })
+      }
+      },
+      error: (error) => {
+        console.error("Errore caricamento CER", error);
+      }
+    });
+  }
+
   submit() {
     this.submitted = true;
-
     if (this.cerForm.invalid) {
       this.cerForm.markAllAsTouched();
 
@@ -64,7 +86,6 @@ export class ModificaCerComponent {
           duration: 3000
         }
       );
-
       return;
     }
 
@@ -75,7 +96,5 @@ export class ModificaCerComponent {
         duration: 2000
       }
     );
-    console.log(this.cerForm.value);
   }
-
 }
