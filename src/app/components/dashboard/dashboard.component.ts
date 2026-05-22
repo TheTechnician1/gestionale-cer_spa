28823a5338bd25f0284224f4d25d2f49eac60e21
Emanuel <emanueldum11@gmail.com>
2026-05-22 15:20:15 +0200

    creazione dashboard ts-service

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
 import { DashboardService } from "../services/dashboard.service";
 import { LiveAnnouncer } from "@angular/cdk/a11y";
 import { MatSort, Sort } from "@angular/material/sort";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatTableDataSource } from "@angular/material/table";
+import { Router } from "@angular/router";
+import { ImpiantoService} from "../services/impianto.service";
+import { DatiEnergeticiService } from "../services/dati-energetici.service";
+import { forkJoin } from "rxjs";
+
+
 
 @Component({
   selector: "app-dashboard",
@@ -14,6 +20,9 @@ export class DashboardComponent implements OnInit {
   constructor(
     private dashboardService: DashboardService,
     private _liveAnnouncer: LiveAnnouncer,
+    private impinatoService: ImpiantoService,
+    private datiEnergeticiService: DatiEnergeticiService,
+    private router: Router
   ) {}
 
   tableCER: string[] = ["ragSociale", "codFisc", "pIva", "comune", "provincia", "regione", "azioni"];
@@ -23,8 +32,18 @@ export class DashboardComponent implements OnInit {
   dataSource = new MatTableDataSource(this.cer);
   sortedData: any[] | undefined;
 
+  anniDisponibili: number[] = [];
+
+  tableColumnsTopCer: string[] = ['ragSociale', 'energiaCondivisa', 'incentivi'];
+  tableColumnsAlert: string[] = ['tipo', 'descrizione'];
+
+  dataSourceTopCer = new MatTableDataSource<any>([]);
+  dataSourceAlert = new MatTableDataSource<any>([]);
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild('paginatorTopCer') paginatorTopCer!: MatPaginator;
+  @ViewChild('sortTopCer') sortTopCer!: MatSort;
 
   filtro = {
     ragSociale: "",
@@ -35,16 +54,36 @@ export class DashboardComponent implements OnInit {
     regione: "",
   };
 
+  filtroDashboard: any = {
+    anno: new Date().getFullYear(),
+    regione: null,
+    provincia: null,
+    comune: null,
+    tipologia: null,
+    statoImpianto: null,
+    idCer: null
+  };
+
   listaFiltrata = [...this.cer];
   isFiltering = false;
 
+  summary: any = null;
+  impiantiPerStato: any[] = [];
+  impiantiPerTipologia: any[] = [];
+  andamentoEnergetico: any[] = [];
+  topCer: any[] = [];
+  alert: any[] = [];
+
   ngOnInit() {
     this.loadCERS();
+    this.caricaDashboard();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+    this.dataSourceTopCer.paginator = this.paginatorTopCer;
+    this.dataSourceTopCer.sort = this.sortTopCer;
   }
 
   loadCERS() {
@@ -86,4 +125,54 @@ export class DashboardComponent implements OnInit {
       this._liveAnnouncer.announce("Sorting cleared");
     }
   }
+
+
+caricaDashboard(): void {
+  forkJoin({
+    summary: this.dashboardService.getSummary(this.filtroDashboard),
+    impiantiPerStato: this.dashboardService.getImpiantiPerStato(this.filtroDashboard),
+    impiantiPerTipologia: this.dashboardService.getImpiantiPerTipologia(this.filtroDashboard),
+    andamentoEnergetico: this.dashboardService.getAndamentoEnergetico(this.filtroDashboard),
+    topCer: this.dashboardService.getTopCer(this.filtroDashboard),
+    alert: this.dashboardService.getAlert(this.filtroDashboard)
+  }).subscribe({
+    next: (data) => {
+      this.summary = data.summary;
+      this.impiantiPerStato = data.impiantiPerStato;
+      this.impiantiPerTipologia = data.impiantiPerTipologia;
+      this.andamentoEnergetico = data.andamentoEnergetico;
+      this.topCer = data.topCer;
+      this.dataSourceTopCer.data = data.topCer;
+      this.alert = data.alert;
+      this.dataSourceAlert.data = data.alert;
+    },
+    error: (err) => console.error('Errore caricamento dashboard', err)
+  });
+}
+
+applicaFiltri(): void {
+  this.caricaDashboard();
+}
+
+resetFiltriDashboard(): void {
+  this.filtroDashboard = {
+    anno: new Date().getFullYear(),
+    regione: null,
+    provincia: null,
+    comune: null,
+    tipologia: null,
+    statoImpianto: null,
+    idCer: null
+  };
+  this.caricaDashboard();
+}
+
+navigaImpianti(statoImpianto?: string): void {
+  if (statoImpianto) {
+    this.router.navigate(['/impianto'], { queryParams: { statoImpianto } });
+  } else {
+    this.router.navigate(['/impianto']);
+  }
+}
+
 }
--- src/app/components/services/dashboard.service.ts
+++ src/app/components/services/dashboard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { ApiRequestOptions, ApiService } from "../../core/services/api.service";
 import { Observable } from "rxjs";
 
+
 @Injectable({
   providedIn: "root",
 })
@@ -12,4 +13,35 @@ export class DashboardService {
     const endpoint = "cer/ricerca";
     return this.api.postLogin<any[]>(endpoint, payload, options);
   }
+
+  getSummary(payload: any, options: ApiRequestOptions = {}): Observable<any> {
+    const endpoint = "api/dashboard/summary";
+    return this.api.get<any>(endpoint, payload, options);
+  }
+
+  getImpiantiPerStato(payload: any, options: ApiRequestOptions = {}): Observable<any[]> {
+    const endpoint = "api/dashboard/impianti-per-stato";
+    return this.api.get<any[]>(endpoint, payload, options);
+  }
+
+  getImpiantiPerTipologia(payload: any, options: ApiRequestOptions = {}): Observable<any[]> {
+    const endpoint = "api/dashboard/impianti-per-tipologia";
+    return this.api.get<any[]>(endpoint, payload, options);
+  }
+
+  getAndamentoEnergetico(payload: any, options: ApiRequestOptions = {}): Observable<any[]> {
+    const endpoint = "api/dashboard/andamento-energetico";
+    return this.api.get<any[]>(endpoint, payload, options);
+  }
+
+  getTopCer(payload: any, options: ApiRequestOptions = {}): Observable<any[]> {
+    const endpoint = "api/dashboard/top-cer";
+    return this.api.get<any[]>(endpoint, payload, options);
+  }
+
+  getAlert(payload: any, options: ApiRequestOptions = {}): Observable<any[]> {
+    const endpoint = "api/dashboard/alert";
+    return this.api.get<any[]>(endpoint, payload, options);
+  }
 }
+
