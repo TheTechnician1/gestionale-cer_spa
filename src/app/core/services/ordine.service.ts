e19beab0992fb8dc57f7b6afa68f058abab862b2
pudduvalentino1998-glitch <pudduvalentino1998@gmail.com>
2026-06-18 15:17:53 +0200

    Gestione errore invio ricevuta

--- src/app/core/services/ordine.service.ts
+++ src/app/core/services/ordine.service.ts
@@ -3,13 +3,16 @@ import { ApiRequestOptions, ApiService } from './api.service';
 import { Observable } from 'rxjs';
 import { Ordine } from '../interfaces/ordine';
 import { ArticoloCarrelloDTO } from '../interfaces/carrello';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdineService {
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService,
+    private toast:ToastService
+  ) { }
 
   visualizzaOrdine(orderId: number|null, options: ApiRequestOptions = {}): Observable<Ordine> {
       const endpoint = `api/orders/${orderId}`;
@@ -22,8 +25,17 @@ export class OrdineService {
   }
 
   invioRicevuta(orderId: number|null, options: ApiRequestOptions = {}) : void{
-      const endpoint = `api/orders/${orderId}/receipt/email`;
-      this.api.post<any>(endpoint, {}, options).subscribe();
+    const endpoint = `api/orders/${orderId}/receipt/email`;
+    this.api.post<any>(endpoint, {}, options).subscribe({
+      next: () => {
+        this.toast.success("Invio ricevuta completato")
+        console.log('Ricevuta inviata correttamente');
+      },
+      error: (err) => {
+        this.toast.error("Errore durante l'invio della ricevuta")
+        console.error('Errore durante l\'invio della ricevuta', err);
+      }
+    });
   }
 
 }
